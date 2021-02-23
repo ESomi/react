a36d141ef01da37a662fbba2db8feadc9333dced
ESomi <sweetsom27@gmail.com>
2021-02-23 19:16:42 +0900

    clear files

---
+++ src/components2/Test.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+class Test extends React.Component {
+    render() {
+        return (
+            <div>
+                This is React test. 
+            </div>
+        );
+    }
+}
+export default Test;
+
+ReactDOM.render(<Test/>, document.getElementById('root'));
+
